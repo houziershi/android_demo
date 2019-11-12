3d5c1035cd1483757d595c3b603ab052b17a19a0
houguokun@lazyaudio.com <Hou1234>
2019-11-12 10:41:26 +0800

    深入理解 下拉刷新和上拉加载更多 系列一

--- app/src/main/java/com/example/admin/myapplication_app/custom/PullableLayout.java
+++ app/src/main/java/com/example/admin/myapplication_app/custom/PullableLayout.java
@@ -4,23 +4,35 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.admin.myapplication_app.R;
+import com.example.admin.myapplication_app.Util;
 
 /**
  * Discription:
  * Created by guokun on 2019/11/11.
  */
 public class PullableLayout extends ViewGroup {
+    private static final String TAG = "PullableLayout";
 
     // ViewGroup的内容高度(不包括header与footer的高度)
     private int mLayoutContentHeight;
+    private int mTouchSlop;
 
     private View mHeader;
     private View mFooter;
+    private float mlastY;
+    private int effectiveScrollY;
+    private TextView tvPullHeader;
+    private ProgressBar pbPullHeader;
+    private TextView tvPullFooter;
 
     public PullableLayout(Context context) {
         this(context, null);
@@ -33,12 +45,19 @@ public class PullableLayout extends ViewGroup {
     public PullableLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         mHeader = LayoutInflater.from(context).inflate(R.layout.header, null);
+        tvPullHeader = mHeader.findViewById(R.id.tv_pullable_header);
+        pbPullHeader = mHeader.findViewById(R.id.pb_pullable_header);
+
         mFooter = LayoutInflater.from(context).inflate(R.layout.footer, null);
+        tvPullFooter = mFooter.findViewById(R.id.tv_pullable_footer);
+        final ViewConfiguration configuration = ViewConfiguration.get(context);
+        mTouchSlop = configuration.getScaledTouchSlop();
+        effectiveScrollY = Util.dip2px(context, 60);
     }
 
     @Override
     protected void onFinishInflate() {
-        Log.i("hgk", " <onFinishInflate>........");
+        Log.i(TAG, "<onFinishInflate>");
         super.onFinishInflate();
         RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams
                 (RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
@@ -50,7 +69,8 @@ public class PullableLayout extends ViewGroup {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        Log.i("hgk", " <onMeasure>........");
+        Log.i(TAG, "<onMeasure>");
+
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         for (int i = 0; i < getChildCount(); i++) {
             final View child = getChildAt(i);
@@ -60,11 +80,11 @@ public class PullableLayout extends ViewGroup {
 
     @Override
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
-        Log.i("hgk","<onLayout>");
+        Log.i(TAG, "<onLayout>");
+
         mLayoutContentHeight = 0;
         for (int i = 0; i < getChildCount(); i++) {
             final View child = getChildAt(i);
-            Log.i("hgk", " <onLayout>........======" + child.getClass().getSimpleName());
             if (child == mHeader) {
                 child.layout(0, 0 - child.getMeasuredHeight(), child.getMeasuredWidth(), 0);
             } else if (child == mFooter) {
@@ -74,5 +94,59 @@ public class PullableLayout extends ViewGroup {
                 mLayoutContentHeight += child.getMeasuredHeight();
             }
         }
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        Log.i(TAG, "<onInterceptTouchEvent>");
+
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.i(TAG, "<onTouchEvent>");
+        final int action = event.getAction();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                mlastY = event.getY();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                final float y = event.getY();
+                final int yDiff = (int) Math.abs(y - mlastY);
+                if (yDiff > mTouchSlop) {
+                    if (mlastY - y < 0) {
+                        //下拉刷新
+                        if (Math.abs(getScrollY()) <= mHeader.getMeasuredHeight() / 2) {
+                            scrollBy(0, (int) (mlastY - y));
+                            mlastY = y;
+                            if (Math.abs(getScrollY()) > effectiveScrollY) {
+                                tvPullHeader.setText("松开刷新");
+                            }
+                        }
+                    } else {
+                        //上拉加载更多
+                        if (Math.abs(getScrollY()) <= mFooter.getMeasuredHeight() / 2) {
+                            scrollBy(0, (int) (mlastY - y));
+                            mlastY = y;
+                            if (Math.abs(getScrollY()) > effectiveScrollY) {
+                                tvPullFooter.setText("松开刷新");
+                            }
+                        }
+                    }
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                if (Math.abs(getScrollY()) < effectiveScrollY) {
+                    scrollBy(0, -getScrollY());
+                } else {
+                    scrollBy(0, -(getScrollY() + effectiveScrollY));
+                    tvPullHeader.setVisibility(View.GONE);
+                    pbPullHeader.setVisibility(View.VISIBLE);
+                }
+                break;
+        }
+
+        return true;
     }
 }
