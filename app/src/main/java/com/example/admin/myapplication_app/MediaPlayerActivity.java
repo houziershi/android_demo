2a693dc2a2901ba7e397d492d6df023ac3931363
houguokun@lazyaudio.com <Hou1234>
2020-11-23 15:14:54 +0800

    删除第三方库

---
+++ app/src/main/java/com/example/admin/myapplication_app/MediaPlayerActivity.java
@@ -0,0 +1,18 @@
+package com.example.admin.myapplication_app;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Discription:
+ * Created by guokun on 2020/11/23.
+ */
+class MediaPlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
